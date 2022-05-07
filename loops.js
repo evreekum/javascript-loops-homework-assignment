c718b48f9073d11d0cfc0c652519364a443a6dd0
evreekum <e.v.reekum@gmail.com>
2022-05-07 17:48:01 +0200

    finished-loops

--- loops.js
+++ loops.js
@@ -6,7 +6,18 @@
 // Hoera!
 // ==========================================
 
+const logHoeraJa = ['Hoera!', 'Hoera!', 'Hoera!'];
+for (let i = 0; i<3; i++) {
+    console.log(logHoeraJa[i]);
+}
 
+/*/!*___Of:___*!/
+
+const logHoera = ['Hoera!'];
+
+for (let i = 0; i<3; i++) {
+    console.log(logHoera);
+}*/
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -17,9 +28,22 @@
 // loop...
 // klaar!
  // ==========================================
+const logLoop = ['loop...', 'loop...', 'loop...', 'loop...', 'Klaar!'];
 
+for (let i = 0; i < logLoop.length; i++) {
+    console.log(logLoop[i]);
 
+}
 
+/*___Of:___*/
+
+for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+        console.log("loop...");
+    } else {
+        console.log("klaar!");
+    }
+}
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
 // Verwachte uitkomsten:
@@ -32,8 +56,18 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
+const evensOdds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 
+for (let i = 0; i < evensOdds.length; i++) {
+    if (evensOdds[i] % 2 === 0) {
+        console.log(evensOdds[i] + ' is even')
+    } else {
+        console.log(evensOdds[i] + ' is oneven')
+    }
+}
+
+/*console.log(evensOdds[2]);*/
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -50,4 +84,16 @@
 // >> 8
 // >> 9
 // ==========================================
+const bonus = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+for (let i = 0; i < bonus.length; i++) {
+    if (bonus[i] < 3) {
+        console.log(bonus[i]);
+    }
+    if (bonus[i] > 2 && bonus[i] < 6) {
+        console.log('>' + bonus[i]);
+    }
+    if (bonus[i] > 5) {
+        console.log('>>' + bonus[i]);
+    }
+}
